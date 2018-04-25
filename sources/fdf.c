02113b8da1128fa48fb3d70a7789924cd67da43d
Benjamin Pisano <benjamin.pisano@icloud.com>
2018-04-25 16:02:47 +0200

    fixes

--- sources/fdf.c
+++ sources/fdf.c
@@ -6,7 +6,7 @@
 /*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/03/28 16:15:51 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2018/04/25 15:21:25 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/04/25 15:57:20 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,10 +23,16 @@ int		main(int ac, char **av)
 	t_env	env;
 
 	if (ac != 2)
+	{
+		ft_printf("Usage: fdf [filename]\n");
 		return (0);
+	}
 	init_env(&env);
 	if (!parse(av[1], &env))
+	{
+		ft_printf("Invalid file");
 		return (0);
+	}
 	rotate(&env, 0, 0, 0);
 	draw(&env);
 	start_loop(&env);
